6418228b2933a687edff7e755bfe8f16b77589dc
Toby Murray <murray.toby@gmail.com>
2016-10-14 12:04:59 -0400

    Use custom event binding

--- src/app/databinding/event-binding.component.ts
+++ src/app/databinding/event-binding.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'fa-event-binding',
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class EventBindingComponent {
+  @Output() clicked = new EventEmitter<string>();
+
   onClicked() {
-    alert('It worked');
+    this.clicked.emit('It works');
   }
 }
